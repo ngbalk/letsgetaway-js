2316237492385a49c44715a8bda188a8a15e2b9c
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro-2.local>
2016-08-17 22:55:37 -0400

    using process.env variables

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.get('/*',function(req,res){
 	res.sendFile(__dirname+'/app/index.html');
 });
 
-app.listen(8080, "127.0.0.1", function () {
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+app.listen(port, ip, function () {
   console.log('Letsgetaway app started on port 8080');
 });
